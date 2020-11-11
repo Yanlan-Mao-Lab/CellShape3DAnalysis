1947293c519adfb4b2c7058980505818233b8c31
Pablo Vicente Munuera <pablovm1990@gmail.com>
2020-11-11 09:25:02 +0000

    Unstructured pipeline

--- visualizeSegmentedStack.m
+++ visualizeSegmentedStack.m
@@ -1,3 +1,5 @@
+
+
 %Read stack
 stackFile = 'Data/190321_RnG4-UASmyrGFP_CELLOTAPE-FILTERPAPER_DISH-3-DISC-1_STACK_SR-2-5X_predictions_gasp_average.tiff';
 [tiff_stack] = readStackFile(stackFile);
@@ -6,32 +8,68 @@ xyScale = 0.0744151;
 zScale = 0.5336974;
 resizingSize = 1;
 
-tiff_stack_trueScale = imresize3(double(tiff_stack), size(tiff_stack)/resizingSize .* [1 1 zScale/xyScale], 'nearest');
+tiff_stack_trueScale = uint16(imresize3(double(tiff_stack), size(tiff_stack)/resizingSize .* [1 1 zScale/xyScale], 'nearest'))-1;
 
-featuresImage = regionprops3(tiff_stack_trueScale-1, 'all');
+clearvars tiff_stack
+
+%featuresImage = regionprops3(tiff_stack_trueScale-1, 'all');
 
 lastCell = max(tiff_stack_trueScale(:));
+background = tiff_stack_trueScale == 0;
 
-infoPerZ = {};
-for numZ = 1:size(tiff_stack_trueScale, 3)
+background_dilated = imdilate(background, strel('sphere', 1));
+outerLayerTissue = uint16(tiff_stack_trueScale>0 & background_dilated) .* tiff_stack_trueScale;
+
+clearvars background_dilated background
+
+outerAreasOfCell = cell(lastCell, 1);
+for numCell = 1:lastCell
+    numCell
+    outerAreasOfCell{numCell} = regionprops3(outerLayerTissue == numCell, 'Volume');
+end
+
+cellfun(@(x) length(x), outerAreasOfCell);
+
+[neighbours] = calculateNeighbours(tiff_stack_trueScale, 4);
+
+% infoPerZ = {};
+% for numZ = 1:size(tiff_stack_trueScale, 3)
+%     numZ
+%     infoPerZ{numZ} = regionprops(tiff_stack_trueScale(:, :, numZ)-1, {'Area', 'MajorAxisLength', 'MinorAxisLength' 'Eccentricity', 'Orientation', 'ConvexArea', 'FilledArea', 'EquivDiameter', 'Solidity', 'Extent', 'Perimeter'});
+% end
+
+cellHeights = zeros(lastCell, length(infoPerZ));
+for numZ = 1:length(infoPerZ)
     numZ
-    infoPerZ{numZ} = regionprops(tiff_stack_trueScale(:, :, numZ)-1, {'Area', 'MajorAxisLength', 'MinorAxisLength' 'Eccentricity', 'Orientation', 'ConvexArea', 'FilledArea', 'EquivDiameter', 'Solidity', 'Extent', 'Perimeter'});
+    actualZ = infoPerZ{numZ};
+    
+    for numCell = 1:length(actualZ)
+        cellHeights(numCell, numZ) = actualZ(numCell).Area;
+    end
 end
 
-outputCSV = cell(lastCell, 1);
-
-for numSeed = 2:lastCell
-    numSeed
-    % Painting each cell
-    [x,y,z] = ind2sub(size(tiff_stack_trueScale),find(tiff_stack_trueScale == numSeed));
-    x = x * resizingSize;
-    y = y * resizingSize;
-    z = z * resizingSize;
-    outputCSV{numSeed} = [x, y, z, repmat(numSeed, [length(x) 1]), x*xyScale, y*xyScale, z*xyScale];
+for numCell = 1:size(cellHeights, 1)
+    numZsOfCell = find(cellHeights(numCell, :));
+    
 end
+find(cellHeights(1, :))
 
-outputCSV_all = vertcat(outputCSV{2:end});
-
-writetable(outputCSV_all, '190321_RnG4-UASmyrGFP_CELLOTAPE-FILTERPAPER_DISH-3-DISC-1_STACK_SR-2-5X_segmentedCells.csv');
+% outputCSV = cell(lastCell, 1);
+% 
+% for numSeed = 2:lastCell
+%     numSeed
+%     % Painting each cell
+%     [x,y,z] = ind2sub(size(tiff_stack_trueScale),find(tiff_stack_trueScale == numSeed));
+%     x = x * resizingSize;
+%     y = y * resizingSize;
+%     z = z * resizingSize;
+%     outputCSV{numSeed} = [x, y, z, repmat(numSeed, [length(x) 1]), x*xyScale, y*xyScale, z*xyScale];
+% end
+% 
+% outputCSV_all = vertcat(outputCSV{2:end});
+% 
+% outputCSV_T = array2table(outputCSV_all);
+% 
+% writetable(outputCSV_T, '190321_RnG4-UASmyrGFP_CELLOTAPE-FILTERPAPER_DISH-3-DISC-1_STACK_SR-2-5X_segmentedCells.csv');
 
 paint3DCells(tiff_stack_trueScale - 1, [], [])
