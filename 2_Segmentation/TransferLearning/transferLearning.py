ed70f0e5b28bdd06e83ed357d0619f97c4432ff2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-01-14 11:27:46 +0000

    Update transferLearning.py
    Adding learner from basic torchvision

--- 2_Segmentation/TransferLearning/transferLearning.py
+++ 2_Segmentation/TransferLearning/transferLearning.py
@@ -1,3 +1,9 @@
 import torchvision.models as models
 
 
+
+learn = Learner(dls, # dataloader
+                flower_model, #pretrained model
+                metrics=[accuracy],
+                opt_func= SGD,
+                loss_func=nn.CrossEntropyLoss())
