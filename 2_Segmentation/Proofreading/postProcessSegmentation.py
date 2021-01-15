34705d026c06204b8f09ed3d192e075ef480d1d7
shuenlim <shuen@live.co.uk>
2021-01-15 19:10:59 +0000

    added Id properties + thresholding histogram

--- 2_Segmentation/Proofreading/postProcessSegmentation.py
+++ 2_Segmentation/Proofreading/postProcessSegmentation.py
@@ -2,12 +2,13 @@
 
 import os, sys
 
-from skimage import io
+from skimage import io, measure
 from PIL.TiffTags import TAGS
 from PIL import Image
 import tifffile as tiff
 import numpy as np
 import napari
+import matplotlib.pyplot as plt
 
 rawFilePath = '201105_NubG4-UASmyrGFP_COVERSLIP-FLAT_DISH-1-DISC-1_STACK.tif'
 segFilePath = '201105_NubG4-UASmyrGFP_COVERSLIP-FLAT_DISH-1-DISC-1_STACK_predictions_gasp_average.tiff'
@@ -45,22 +46,52 @@ segmentedImg = segmentedImg - 1;
 uniqueIds=np.unique(segmentedImg)
 maxId = uniqueIds.max()
 
-#Replaces ID with 0 if ID is in a fewer number of slices than threshold
-#VERY slow!!! >> hrs
+#Measures properties of labelled regions
+props = measure.regionprops(segmentedImg)
 
-threshold = 20
+#Array of Id and area (same as volume?)
+a = np.zeros((maxId,2))
 
+for i in range(maxId):
+    a[i,1] = props[i].area
+    a[i,0] = i
+
+#Threshold segments
+areaMean = np.mean(a)
+areaStd = np.std(a)
+smallThreshold = 10000
+bigThreshold = 600000
+
+aThreshold = np.empty((0,2), int)
+IdsToRemove = np.empty((0,2),int)
+
+for i in range(maxId):
+    if a[i,1] > smallThreshold and a[i,1] < bigThreshold:
+        aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0)
+    else:
+        IdsToRemove = np.append(IdsToRemove, np.array([a[i,:]]), axis=0)
+        
+#Plot histogram of thresholded areas
+plt.hist(aThreshold[:,1], bins=30)
+plt.show()
+
+#Keep only thresholded Ids in post processed image
 postProcessImg=segmentedImg.copy()
 
 for Id in range(maxId):
     print(Id)
-    count = 0
-    for s in range(postProcessImg.shape[0]):
-        if Id in postProcessImg[s,:,:]:
-            count += 1
-    if count < threshold:
+    if Id in IdsToRemove[:,0]:
         postProcessImg[postProcessImg==Id]=0
 
+#Replaces ID with 0 if ID is in a fewer number of slices than threshold
+#VERY slow!!! >> hrs
+
+#for Id in range(maxId):
+#    print(Id)
+#    count = len(np.unique(np.where(segmentedImg==Id)[0]))
+#    if count < threshold:
+#        postProcessImg[postProcessImg==Id]=0
+        
 #Visualise raw, segmented and postprocessed image using napari
 #Napari viewer with raw and segment images as layers
 with napari.gui_qt():
