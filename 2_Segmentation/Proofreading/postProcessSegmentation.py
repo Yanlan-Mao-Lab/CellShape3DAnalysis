15bdd857c7e577c052560c07f8abd8590feaa76f
shuenlim <shuen@live.co.uk>
2021-01-19 12:23:58 +0000

    thresholding
    ids of small/big cells outside thresholds changed to 0 in postProcessImg

--- 2_Segmentation/Proofreading/postProcessSegmentation.py
+++ 2_Segmentation/Proofreading/postProcessSegmentation.py
@@ -49,7 +49,7 @@ maxId = uniqueIds.max()
 #Measures properties of labelled regions
 props = measure.regionprops(segmentedImg)
 
-#Array of Id and area (same as volume?)
+#Array of Id and area
 a = np.zeros((maxId,2))
 
 for i in range(maxId):
@@ -57,47 +57,49 @@ for i in range(maxId):
     a[i,0] = i
 
 #Threshold segments
-areaMean = np.mean(a)
-areaStd = np.std(a)
 smallThreshold = 10000
-bigThreshold = 600000
+bigThreshold = 1000000
+backgroundThreshold = 100000000
 
-aThreshold = np.empty((0,2), int)
-IdsToRemove = np.empty((0,2),int)
+aThreshold = np.empty((0,2))
+IdsToRemove = np.empty((0,2))
+backgroundIds = np.empty((0,2))
 
 for i in range(maxId):
-    if a[i,1] > smallThreshold and a[i,1] < bigThreshold:
-        aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0)
+    if a[i,1] > backgroundThreshold:
+        backgroundIds= np.append(backgroundIds, np.array([a[i,:]]), axis=0).astype(np.uint32)
+    elif a[i,1] > smallThreshold and a[i,1] < bigThreshold:
+        aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0).astype(np.uint32)
     else:
-        IdsToRemove = np.append(IdsToRemove, np.array([a[i,:]]), axis=0)
-        
+        IdsToRemove = np.append(IdsToRemove, np.array([a[i,:]]), axis=0).astype(np.uint32)
+
 #Plot histogram of thresholded areas
-plt.hist(aThreshold[:,1], bins=30)
+plt.hist(aThreshold[:,1], bins=50)
+plt.xlabel('Segment area')
+plt.ylabel('Frequency density')
 plt.show()
+
+
 
 #Keep only thresholded Ids in post processed image
 postProcessImg=segmentedImg.copy()
 
-for Id in range(maxId):
-    print(Id)
-    if Id in IdsToRemove[:,0]:
-        postProcessImg[postProcessImg==Id]=0
+for i in range(len(IdsToRemove)):
+    Id = IdsToRemove[i,0]
+    postProcessImg[props[Id].coords[:,0],props[Id].coords[:,1],props[Id].coords[:,2]] = 0
 
-#Replaces ID with 0 if ID is in a fewer number of slices than threshold
-#VERY slow!!! >> hrs
+#Runs out of memory with large background Ids
+#Method above takes ages with backgroundIds but method below takes ages with IdsToRemove ?
+for i in range(len(backgroundIds)):
+    Id = backgroundIds[i,0]
+    postProcessImg[postProcessImg==Id]=0
 
-#for Id in range(maxId):
-#    print(Id)
-#    count = len(np.unique(np.where(segmentedImg==Id)[0]))
-#    if count < threshold:
-#        postProcessImg[postProcessImg==Id]=0
-        
 #Visualise raw, segmented and postprocessed image using napari
 #Napari viewer with raw and segment images as layers
 with napari.gui_qt():
     viewer = napari.view_image(rawImg, rgb=False, colormap='green', blending='additive')
     viewer.add_image(segmentedImg, rgb=False, colormap='magenta', blending='additive')
-    viewer.add_image(postProcessImg, rgb=False)
+    viewer.add_image(postProcessImg, rgb=False, blending='additive')
 
 #Separate window with post processed image?
 #with napari.gui_qt():
