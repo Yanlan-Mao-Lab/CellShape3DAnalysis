25096a28053df8fe2fac2a4280d2248e89f4bdb7
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-01-21 14:58:17 +0000

    dont know

--- 2_Segmentation/TransferLearning/transferLearning.py
+++ 2_Segmentation/TransferLearning/transferLearning.py
@@ -1,9 +1,6 @@
-import torchvision.models as models
+import torch
 
-
-
-learn = Learner(dls, # dataloader
-                flower_model, #pretrained model
-                metrics=[accuracy],
-                opt_func= SGD,
-                loss_func=nn.CrossEntropyLoss())
+checkpoint_path = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/CellShape3DAnalysis/2_Segmentation/TransferLearning/PreTrainedModels/unet3d-arabidopsis-ovules-confocal-ds2x.pytorch'
+state = torch.load(checkpoint_path, map_location='cpu')
+print (state)
+state['model_state_dict']
