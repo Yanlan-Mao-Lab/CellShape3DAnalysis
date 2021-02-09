f7a4672b3a59de5a242feb991bf6d9549780acc3
shuenlim <shuen@live.co.uk>
2021-02-09 15:00:11 +0000

    resize images + background filtering separated

--- 2_Segmentation/Proofreading/postProcessSegmentation.py
+++ 2_Segmentation/Proofreading/postProcessSegmentation.py
@@ -3,6 +3,7 @@
 import os, sys
 
 from skimage import io, measure, segmentation
+from skimage.transform import resize
 from PIL.TiffTags import TAGS
 from PIL import Image
 import tifffile as tiff
@@ -40,57 +41,82 @@ def raw_parameters(rawFilePath):
 def load_raw_seg_images(rawFilePath, segFilePath):
     # Read segmented image to remove first biggest label
     rawImg = io.imread(rawFilePath);
-    
+    resize_rawImg = resize(rawImg, (rawImg.shape[0], 512, 512),
+                           order=0,preserve_range=True, anti_aliasing=False).astype(np.uint16)
     segmentedImg = io.imread(segFilePath);
     segmentedImg = segmentedImg - 1;
+    resize_segmentedImg = resize(segmentedImg, (segmentedImg.shape[0], 512, 512),
+                           order=0,preserve_range=True, anti_aliasing=False).astype(np.uint16)
     
-    uniqueIds=np.unique(segmentedImg)
-    maxId = uniqueIds.max()
-    
-    return rawImg, segmentedImg, maxId
-
-#Threshold segments to remove too small/big segments and background
-def threshold_segments(segmentedImg, maxId,
-                       smallThreshold, bigThreshold, backgroundThreshold,
-                       hist=False, nbins=30):
-    
-    #Measures properties of labelled regions
-    #Labels with 0 ignored so indices are n-2 from rawImg or n-1 from segmentedImg
+    #uniqueIds=np.unique(segmentedImg)
+    #maxId = uniqueIds.max()
+     
     props = measure.regionprops(segmentedImg)
+
+    return resize_rawImg, resize_segmentedImg, props
+
+def area_array(props):
     
-    #Array of Id and area
-    a = np.zeros((maxId,2))
+    a = np.zeros((len(props),2))
     
-    for i in range(maxId):
+    for i in range(len(props)):
         a[i,1] = props[i].area
         a[i,0] = props[i].label
         
-    aThreshold = np.empty((0,2))
-    IdsToRemove = np.empty((0,2))
-    backgroundIds = np.empty((0,2))
-    
-    #Thresholding areas
-    for i in range(maxId):
-        if a[i,1] > backgroundThreshold:
-            backgroundIds= np.append(backgroundIds, np.array([a[i,:]]), axis=0).astype(np.uint32)
-        elif a[i,1] > smallThreshold and a[i,1] < bigThreshold:
-            aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0).astype(np.uint32)
-        else:
-            IdsToRemove = np.append(IdsToRemove, np.array([a[i,:]]), axis=0).astype(np.uint32)
+    return a.astype(np.uint32)
 
-    #Keep only thresholded Ids in post processed image
-    postProcessImg=segmentedImg.copy()
+def remove_background(segmentedImg, props, hist=True, nbins=30):
     
-    for i in range(len(IdsToRemove)):
-        Id = IdsToRemove[i,0]-1
-        postProcessImg[props[Id].coords[:,0],props[Id].coords[:,1],props[Id].coords[:,2]] = 0
+    a = area_array(props)
+
+    backgroundIds = np.empty((0,2))
+
+    for i in range(len(props)):
+        if a[i,1] > 100000000:
+            backgroundIds= np.append(backgroundIds, np.array([a[i,:]]), axis=0).astype(np.uint32)
     
-    #Runs out of memory with large background Ids
-    #Method above takes ages with backgroundIds but method below takes ages with IdsToRemove ?
     for i in range(len(backgroundIds)):
         Id = backgroundIds[i,0]
-        postProcessImg[postProcessImg==Id] = 0
+        segmentedImg[segmentedImg==Id] = 0
     
+    new_props = measure.regionprops(segmentedImg)
+    
+    new_a = area_array(new_props)
+        
+    #Plot histogram
+    if hist == True:
+        plt.hist(new_a[:,1], bins=nbins)
+        plt.xlabel('Segment area')
+        plt.ylabel('Frequency density')
+        plt.show()
+        
+    return segmentedImg, new_props
+
+#Threshold segments to remove too small/big segments and background
+def threshold_segments(segmentedImg, props, smallThreshold, bigThreshold, hist=False, nbins=30):
+    
+    #Measures properties of labelled regions
+    #Labels with 0 ignored so indices are n-2 from rawImg or n-1 from segmentedImg
+    
+    #Array of Id and area
+    a = area_array(props)
+   
+    aThreshold = np.empty((0,2))
+    IdsToRemove = np.empty((0,1))
+    
+    for i in range(len(props)):
+        if a[i,1] > smallThreshold and a[i,1] < bigThreshold:
+            aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0)
+        else:
+            IdsToRemove = np.append(IdsToRemove, i)
+            
+    postProcessImg=rem_background_segmentedImg.copy()
+    
+    for i in range(len(IdsToRemove)):
+        Id = int(IdsToRemove[i])
+        postProcessImg[new_props[Id].coords[:,0],new_props[Id].coords[:,1],new_props[Id].coords[:,2]] = 0
+    
+    #Plot histogram
     if hist == True:
         plt.hist(aThreshold[:,1], bins=nbins)
         plt.xlabel('Segment area')
@@ -113,13 +139,16 @@ def z_slice_hist(postProccessImg,nbins=30):
     plt.show()
 
 
+#Use functions
+
 rawFilePath = '201105_NubG4-UASmyrGFP_COVERSLIP-FLAT_DISH-1-DISC-1_STACK.tif'
 segFilePath = '201105_NubG4-UASmyrGFP_COVERSLIP-FLAT_DISH-1-DISC-1_STACK_predictions_gasp_average.tiff'
 
-rawImg, segmentedImg, maxId = load_raw_seg_images(rawFilePath, segFilePath)
+rawImg, segmentedImg, props = load_raw_seg_images(rawFilePath, segFilePath)
 
-postProcessImg = threshold_segments(segmentedImg, maxId,
-                                    10000, 1000000, 100000000, False)
+rem_background_segmentedImg, new_props = remove_background(segmentedImg, props, False)
+
+postProcessImg = threshold_segments(rem_background_segmentedImg, new_props, 1000, 110000, True)
 
 #Visualise raw, segmented and postprocessed image using napari
 #Napari viewer with raw and segment images as layers
@@ -127,4 +156,3 @@ with napari.gui_qt():
     viewer = napari.view_image(rawImg, rgb=False, colormap='green', blending='additive')
     viewer.add_image(segmentedImg, rgb=False, colormap='magenta', blending='additive')
     viewer.add_image(postProcessImg, rgb=False, blending='additive')
-
