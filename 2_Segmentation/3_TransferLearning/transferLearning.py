45d0f1370d4eebb75ca5e6634e66939a9b054060
Pablo Vicente Munuera <pablovm1990@gmail.com>
2021-02-18 09:05:54 +0000

    Another way of performing transfer learning

--- 2_Segmentation/3_TransferLearning/transferLearning.py
+++ 2_Segmentation/3_TransferLearning/transferLearning.py
@@ -1,6 +1,29 @@
+import numpy as np
 import torch
+from unet import UNet3D
 
-checkpoint_path = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/CellShape3DAnalysis/2_Segmentation/TransferLearning/PreTrainedModels/unet3d-arabidopsis-ovules-confocal-ds2x.pytorch'
-state = torch.load(checkpoint_path, map_location='cpu')
-print (state)
-state['model_state_dict']
+with torch.no_grad():
+    state = torch.load('/home/pablo/.plantseg_models/initialPoint/best_checkpoint.pytorch')
+
+    net = UNet3D(in_channels=1,
+                 out_channels=1,
+                 f_maps=[32, 64, 128, 256],
+                 testing=True)
+
+    net.load_state_dict(state)
+
+    # load and normalize the input
+    im = np.load('test_input.npz')
+    im = im['arr_0'].astype('float32')
+    im -= im.mean()
+    im /= im.std()
+
+    # forward pass
+    inp = torch.from_numpy(im)
+    out = net(inp)
+    learn = Learner(dls, # dataloader
+                flower_model,
+                metrics=[accuracy],
+                opt_func= SGD,
+                loss_func=nn.CrossEntropyLoss())
+    out = out.cpu().numpy()
