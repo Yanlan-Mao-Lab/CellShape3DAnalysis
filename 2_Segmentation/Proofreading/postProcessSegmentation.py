c7d38d8ea199966f528b815d2caefa13b1789988
shuenlim <shuen@live.co.uk>
2021-03-05 10:57:39 +0000

    save as hdf5 function added + skimage.morphology added

--- 2_Segmentation/Proofreading/postProcessSegmentation.py
+++ 2_Segmentation/Proofreading/postProcessSegmentation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
 import os, sys
-
-from skimage import io, measure, segmentation
-from skimage.transform import resize
-from PIL.TiffTags import TAGS
-from PIL import Image
+import h5py
 import tifffile as tiff
+from PIL.TiffTags import TAGS
 import numpy as np
-import napari
 import matplotlib.pyplot as plt
+from skimage import io, measure, morphology, segmentation
+from skimage.transform import resize
+from PIL import Image
+import napari
+
 
 # Obtain raw image parameters: zSpacing, xResolution and yResolution
 def raw_parameters(rawFilePath):
@@ -42,7 +43,8 @@ def load_raw_seg_images(rawFilePath, segFilePath, resize_img=True):
     # Read segmented image to remove first biggest label
     rawImg = io.imread(rawFilePath);
 
-    segmentedImg = io.imread(segFilePath);
+    h5File = h5py.File(segFilePath, 'r')
+    segmentedImg = np.array(h5File.get('segmentation'))
     segmentedImg = segmentedImg - 1;
     
     if resize_img == True:
@@ -76,7 +78,7 @@ def calculate_cell_heights(props):
         cell_heights[i,1] = props[i].bbox[3]-props[i].bbox[0]
         
     return cell_heights
-  
+
 
 def remove_background(segmentedImg, props, hist=True, nbins=30):
     
@@ -105,18 +107,19 @@ def remove_background(segmentedImg, props, hist=True, nbins=30):
         
     return segmentedImg, new_props
 
-def threshold_segments(segmentedImg, props, lower_percentile, higher_percentile, hist=False, nbins=30):
+def threshold_segments(segmentedImg, props, lower_percentile, hist=False, nbins=30):
     
     cell_heights = calculate_cell_heights(props)
         
     smallThreshold = np.percentile(cell_heights[:,1], lower_percentile)
-    bigThreshold = np.percentile(cell_heights[:,1], higher_percentile)
+    
+    #skimage.morphology.remove_small_objects
     
     IdsThresholded = np.empty((0,2))
     IdsToRemove = np.empty((0,1))
     
     for i in range(len(props)):
-        if cell_heights[i,1] > smallThreshold and cell_heights[i,1] < bigThreshold:
+        if cell_heights[i,1] > smallThreshold:
             IdsThresholded = np.append(IdsThresholded, np.array([cell_heights[i,:]]), axis=0)
         else:
             IdsToRemove = np.append(IdsToRemove, i)
@@ -138,30 +141,9 @@ def threshold_segments(segmentedImg, props, lower_percentile, higher_percentile,
     
 
 #Threshold segments to remove too small/big segments and background
-def manual_threshold_segments(segmentedImg, props, smallThreshold, bigThreshold, hist=False, nbins=30):
+def manual_threshold_segments(segmentedImg, smallThreshold, hist=False, nbins=30):
 
-    
-    #Measures properties of labelled regions
-    #Labels with 0 ignored so indices are n-2 from rawImg or n-1 from segmentedImg
-
-    
-    #Array of Id and area
-    a = area_array(props)
-   
-    aThreshold = np.empty((0,2))
-    IdsToRemove = np.empty((0,1))
-    
-    for i in range(len(props)):
-        if a[i,1] > smallThreshold and a[i,1] < bigThreshold:
-            aThreshold = np.append(aThreshold, np.array([a[i,:]]), axis=0)
-        else:
-            IdsToRemove = np.append(IdsToRemove, i)
-            
-    thresholdImg=rem_background_segmentedImg.copy()
-    
-    for i in range(len(IdsToRemove)):
-        Id = int(IdsToRemove[i])
-        thresholdImg[new_props[Id].coords[:,0],new_props[Id].coords[:,1],new_props[Id].coords[:,2]] = 0
+    thresholdImg = morphology.remove_small_objects(segmentedImg, smallThreshold)
     
     #Plot histogram
     if hist == True:
@@ -186,39 +168,60 @@ def z_slice_hist(Img,nbins=30):
     plt.show()
 
 
+def save_hdf5(segFilePath, zSpacing, xResolution, yResolution):
+    
+    postProcessFilePath = segFilePath.rstrip('predictions_gasp_average.h5') 
+    postProcessFilePath += '_postprocessed.h5'
+    postProcessFile = h5py.File(postProcessFilePath, "w")
+    segmentation = postProcessFile.create_dataset("segmentation", data=watershedImg, dtype='uint16')
+    
+    segmentation.attrs['zSpacing'] = zSpacing
+    segmentation.attrs['xResolution'] = [xResolution.numerator,xResolution.denominator]
+    segmentation.attrs['yResolution'] = [yResolution.numerator,yResolution.denominator]
+    postProcessFile.close()
+
+
 #Use functions
-
-rawFilePath = 'Data/Original/Rob/Part2_Decon_c1_t1.tif'
-segFilePath = 'Data/Original/Rob/Part2_Decon_c1_t1_predictions_best.tiff'
-
-rawImg, segmentedImg, props = load_raw_seg_images(rawFilePath, segFilePath)
 
 #rawImg, segmentedImg, props = load_raw_seg_images(sys.argv[1], sys.argv[2], True)
 
+rawFilePath = 'Data/Original/Rici/201105_NubG4-UASmyrGFP-UASRokRNAi_COVERSLIP-FLAT_DISH-2-DISC-1_STACK.tif'
+segFilePath = 'Data/Original/Rici/201105_NubG4-UASmyrGFP-UASRokRNAi_COVERSLIP-FLAT_DISH-2-DISC-1_STACK_predictions_gasp_average.h5'
+
+#rawFilePath = 'Data/Original/Rob/Part2_Decon_c1_t1.tif'
+#segFilePath = 'Data/Original/Rob/Part2_Decon_c1_t1_predictions_best.tiff'
+
+rawImg, segmentedImg, props = load_raw_seg_images(rawFilePath, segFilePath)
+zSpacing, xResolution, yResolution = raw_parameters(rawFilePath)
 rem_background_segmentedImg, new_props = remove_background(segmentedImg, props, False)
 
 #thresholdImg = manual_threshold_segments(rem_background_segmentedImg, new_props, 1000, 110000, False)
-thresholdImg = threshold_segments(rem_background_segmentedImg, new_props, 2, 99.999, False)
 
-#thresholdImg = thresholdImg + 1
+#RICI
+thresholdImg = threshold_segments(rem_background_segmentedImg, new_props, 10, False)
+
+#ROB
+#thresholdImg = threshold_segments(rem_background_segmentedImg, new_props, 30, 99.999, False)
+
 watershedImg = segmentation.watershed(rawImg, thresholdImg, watershed_line = False)
-#watershed_props = measure.regionprops(watershedImg)
+
+# Reassign background IDs
 watershedImg = watershedImg -1
-
-# Saves post processed output as tif
-tiff.imsave('postProcessImg.tif', watershedImg)
-
 thresholdImg[thresholdImg==1] = 0
 
-#https://github.com/taketwo/glasbey
+# Saves post processed output as h5
+
+
+#tiff.imsave(postProcessFilePath,'.f', watershedImg)
 
 #Visualise raw, segmented, thresholded and watershed image using napari
 #Napari viewer with raw and segment images as layers
 with napari.gui_qt():
     viewer = napari.view_image(rawImg, rgb=False, colormap='green', blending='additive')
-    viewer.add_image(segmentedImg, rgb=False, colormap='magenta', blending='additive')
+    #viewer.add_image(segmentedImg, rgb=False, colormap='magenta', blending='additive')
+    viewer.add_labels(segmentedImg, name='PlantSeg')
     #viewer.add_image(thresholdImg, rgb=False, blending='additive')
-    viewer.add_labels(thresholdImg, name='threshold')
+    viewer.add_labels(thresholdImg, name='thresholded')
     #viewer.add_image(watershedImg, rgb=False, blending='additive')
     viewer.add_labels(watershedImg, name='watershed')
     
